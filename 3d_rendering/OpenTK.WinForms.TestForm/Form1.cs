eee897ba58e696169341f23f4c6f6dadb1654819
Greedun <fogreedonr1405@gmail.com>
2022-02-04 11:44:55 +0900

    init and objFileRead and render

---
+++ 3d_rendering/OpenTK.WinForms.TestForm/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Windows.Forms;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+
+namespace OpenTK.WinForms.TestForm
+{
+	public partial class Form1 : Form
+	{
+        private Timer _timer = null!;
+        private float _angle = 0.0f;
+
+        public Form1()
+		{
+			InitializeComponent();
+		}
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this,
+                "This demonstrates a simple use of the new OpenTK 4.x GLControl.",
+                "GLControl Test Form",
+                MessageBoxButtons.OK);
+        }
+
+        private void glControl_Load(object? sender, EventArgs e)
+        {
+            // Make sure that when the GLControl is resized or needs to be painted,
+            // we update our projection matrix or re-render its contents, respectively.
+            glControl.Resize += glControl_Resize;
+            glControl.Paint += glControl_Paint;
+
+            // Redraw the screen every 1/20 of a second.
+            _timer = new Timer();
+            _timer.Tick += (sender, e) =>
+            {
+                _angle += 0.5f;
+                Render();
+            };
+            _timer.Interval = 50;   // 1000 ms per sec / 50 ms per frame = 20 FPS
+            _timer.Start();
+
+            // Ensure that the viewport and projection matrix are set correctly initially.
+            glControl_Resize(glControl, EventArgs.Empty);
+        }
+
+        private void glControl_Resize(object? sender, EventArgs e)
+        {
+            glControl.MakeCurrent();
+
+            if (glControl.ClientSize.Height == 0)
+                glControl.ClientSize = new System.Drawing.Size(glControl.ClientSize.Width, 1);
+
+            // GL.Viewport(0, 0, glControl.ClientSize.Width, glControl.ClientSize.Height);
+            GL.Viewport(0, 0, glControl.ClientSize.Width, glControl.ClientSize.Height);
+
+            float aspect_ratio = Math.Max(glControl.ClientSize.Width, 1) / (float)Math.Max(glControl.ClientSize.Height, 1);
+            Matrix4 perpective = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspect_ratio, 1, 64);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perpective);
+        }
+
+        private void glControl_Paint(object sender, PaintEventArgs e)
+        {
+            Render();
+        }
+
+        private void Render()
+        {
+            var test_xyz_arr = new float[,] { };
+            var test_rgb_arr = new int[,] { };
+
+
+            glControl.MakeCurrent();
+
+            GL.ClearColor(Color4.MidnightBlue);
+            GL.Enable(EnableCap.DepthTest);
+            GL.PointSize(10.0f);
+
+            Matrix4 lookat = Matrix4.LookAt(20, 20, 90, 0, 0, 85, 0,1,3);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref lookat);
+
+            // GL.Rotate(_angle, 0.0f, 1.0f, 0.0f);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.Begin(BeginMode.Points);
+
+            GL.Color4(Color4.Silver);
+            //-13.124 1.000 84.042
+            GL.Vertex3(-13.14f, 1.0f, 84.042f);
+            /*GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);*/
+
+            /*GL.Color4(Color4.Honeydew);
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+
+            GL.Color4(Color4.Moccasin);
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+
+            GL.Color4(Color4.IndianRed);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+
+            GL.Color4(Color4.PaleVioletRed);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+
+            GL.Color4(Color4.ForestGreen);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+
+*/
+            test_xyz_arr = objFile_Render.objFile_Render.ReadXyz();
+            test_rgb_arr = objFile_Render.objFile_Render.ReadRgb();
+            objFile_Render.objFile_Render.xyzrgb_Render(test_xyz_arr, test_rgb_arr);
+
+            GL.End();
+
+            glControl.SwapBuffers();
+        }
+    }
+}
---
+++ 3d_rendering/OpenTK.WinForms.TestForm/objFile_Render.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using OpenTK.Mathematics;
+using OpenTK.Graphics.OpenGL;
+
+namespace objFile_Render
+{
+    public class objFile_Render
+    {
+        public static float[,] ReadXyz()
+        {
+            string path = @"C:\Users\CTI\Desktop\scanControlTest\test2.asc";
+            string[] textValue = File.ReadAllLines(path);
+            float[, ] xyz = new float[textValue.Length, 3];
+           
+            for (int i = 0; i < textValue.Length; i++)
+            {
+                try
+                {
+                    string[] SpaceSplit = textValue[i].Split(' ');
+                    for (int j = 0; j < SpaceSplit.Length; j++)
+                    {
+                        if (j < 3)
+                        {
+                            xyz[i, j] = float.Parse(SpaceSplit[j]);
+                        }
+                    }
+                }
+                catch {; }
+            }
+
+            return xyz;
+            // modified test
+        }
+
+        public static int[,] ReadRgb()
+        {
+            string path = @"C:\Users\CTI\Desktop\scanControlTest\test2.asc";
+            string[] textValue = File.ReadAllLines(path);
+            float[,] rgb = new float[textValue.Length, 3];
+            
+            for (int i = 0; i < textValue.Length; i++)
+            {
+                try
+                {
+                    string[] SpaceSplit = textValue[i].Split(' ');
+                    for (int j = 0; j < SpaceSplit.Length; j++)
+                    {
+                        if (j > 2)
+                        {
+                            rgb[i, j - 3] = float.Parse(SpaceSplit[j]);
+                        }
+                    }
+                }
+                catch {; }
+            }
+
+            return rgb;
+        }
+
+        public static void xyzrgb_Render(float[,] xyz_arr, int[,] rgb_arr)
+        {
+            int xyz_l = (xyz_arr.Length) / 3;
+            int rgb_l = (rgb_arr.Length) / 3;
+
+            // floating오차 해결 방법 찾아보기
+            for (int i = 0; i < rgb_l; i++)
+            {
+                GL.Color4(Color4.Silver);
+                GL.Vertex3(xyz_arr[i, 0], xyz_arr[i, 1], xyz_arr[i, 2]);
+            }
+        }
+    }
+}
